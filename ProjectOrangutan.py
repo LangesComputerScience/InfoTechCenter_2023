d07933f0442d2a3dfc608e87827ba9cd6ffa0dbe
Lange <langescomputerscience@gmail.com>
2023-10-30 08:27:13 -0400

    Add milesToGasStationQuarterTank variable -- Stable

--- ProjectOrangutan.py
+++ ProjectOrangutan.py
@@ -14,6 +14,4 @@ def gasLevelGauge():
 #Function will call the gasLevelGauge to determine gas level and then find a close gas station if low
 def gasLevelAlert():
     milesToGasStationsLow = round(random.uniform(1,25),1)
-    print(milesToGasStationsLow)
-
-gasLevelAlert()
+    milesToGasStationsQuarterTank = round(random.uniform(25.1, 50), 1)
